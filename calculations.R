684abe86225ab27c0d6256475ea7bb5ef5234cb0
Frederick Thielen <frederick@Fredericks-MacBook-Air.local>
2021-10-29 13:49:43 +0200

    version to incorporate round 3

--- calculations.R
+++ calculations.R
@@ -1,8 +1,47 @@
 library("tidyverse")
+library(googlesheets4)
 options(scipen = 999,
         digits = 6)
 
-df_res <- read_sheet(ss = g_sheet, sheet = "responses") %>% 
+g_sheet <- "1VgbqTeaP0L8h0WL-PMcGU9l7kWs2e1BVVYyeJmaEPlE"
+gs4_auth(email = "eshpm.serious.game@gmail.com")
+
+
+# df_res <- read_sheet(ss = g_sheet, sheet = "responses") %>% 
+#   filter(!is.na(wg)) %>% 
+#   replace(is.na(.), 0) %>% 
+#   pivot_longer(cols = starts_with("r"),
+#                names_prefix = "r*",
+#                names_to = "round", values_to = "val") %>% 
+#   mutate(type = str_sub(round, 2, 3),
+#          round = str_sub(round, 1, 1)) %>% 
+#   pivot_wider(names_from = type, values_from = val) %>% 
+#   mutate(round = as.numeric(round))
+
+new_col_names <- c("time", "wg", "gr", 
+               "r1n1",
+               "r1p1",
+               "r1n2",
+               "r1p2",
+               "r1n3",
+               "r1p3",
+               "r2n1",
+               "r2p1",
+               "r2n2",
+               "r2p2",
+               "r2n3",
+               "r2p3",
+               "r3n1",
+               "r3p1",
+               "r3n2",
+               "r3p2",
+               "r3n3",
+               "r3p3")
+
+df_res <- read_sheet(ss = g_sheet, sheet = "Form responses 1")
+names(df_res) <- new_col_names
+  
+df_res <- df_res %>%   
   filter(!is.na(wg)) %>% 
   replace(is.na(.), 0) %>% 
   pivot_longer(cols = starts_with("r"),
@@ -12,6 +51,10 @@ df_res <- read_sheet(ss = g_sheet, sheet = "responses") %>%
          round = str_sub(round, 1, 1)) %>% 
   pivot_wider(names_from = type, values_from = val) %>% 
   mutate(round = as.numeric(round))
+
+
+
+
 
 df_set <- read_sheet(ss = g_sheet, sheet = "settings")
 
@@ -41,6 +84,9 @@ res <- df_res %>%
   mutate(n_pat_tx = n1 + n2  + n3,
          n_pat_notx =  n_pats - n_pat_tx,
          budget_impact = n1 * p1 + n2 * p2 + n3 * p3,
+         man_sales = ifelse(round == 3,
+                            n2 * p2 + n3 * p3,
+                            budget_impact),
          gov_balance = gov_max_budget - budget_impact,
          qalys_gained = n_pat_tx * qaly_yr,
          crowd_out = (gov_max_budget - budget_impact)/wtp,
@@ -63,111 +109,114 @@ res <- df_res %>%
                                    val_hi = n_pats * list_price,
                                    pts_lo = 0,
                                    pts_phi = 10,
-                                   val_new = budget_impact),
+                                   val_new = man_sales),
          pts_man_expected = pt_fun_log(val = c(n_pats * wtp,
                                                n_pats * list_price * sales_expect,
                                                n_pats * list_price),
                                        pts = c(0, 10, 12),
-                                       val_new = budget_impact
+                                       val_new = man_sales
                                        )) %>% 
   ungroup() %>% 
   mutate(gov_total = round(
     pts_qaly_gov * 0.6 + pts_pop_gov * 0.4, 1),
     man_total = round(pts_man_sales * 0.6 + pts_man_expected * 0.4, 1),
     gov_total = case_when(gov_total < 0 ~ 0,
-                          gov_total > 10 ~ 10),
+                          gov_total > 10 ~ 10,
+                          TRUE ~ gov_total),
     man_total = case_when(man_total < 0 ~ 0,
-                          man_total > 10 ~ 10)) %>%
+                          man_total > 10 ~ 10,
+                          TRUE ~ man_total)
+    
+    ) %>%
   data.frame()
 
 res
 
 
-
 # Analysis ----------------------------------------------------------------
-
-
-dat_plot <- res %>% 
-  pivot_longer(c(gov_total, man_total),
-               names_to = "Institution",
-               values_to = "Points") %>% 
-  mutate(Institution = ifelse(Institution == "gov_total",
-                              "Government",
-                              "Pharma"))
-
-# Overall
-ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
-  geom_boxplot() +
-  theme_bw() +
-  ggtitle("Overall distribution of points")
-
-# Per tutor group
-ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
-  geom_boxplot() +
-  theme_bw() +
-  facet_wrap(~ wg) +
-  ggtitle("Overall distribution of points per tutor group")
-
-# Per subgroup
-ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
-  geom_boxplot() +
-  theme_bw() +
-  facet_wrap(~ gr) +
-  ggtitle("Overall distribution of points by subgroup")
-
-# Per case
-ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
-  geom_boxplot() +
-  theme_bw() +
-  facet_grid(round ~ gr) +
-  ggtitle("Overall distribution of points by case")
-
-res %>% 
-  arrange(gr) %>% 
-  select(gr, gov_total, man_total)
-
-# ROUDN toevoegen
-
-# Which group did overspend the government budget?
-res %>% 
-  filter(gov_balance < 0) %>% 
-  group_by(gr) %>% 
-  summarise(n = n()) 
-
-# Not treated
-res %>% 
-  filter(n_pat_notx > 0) %>% 
-  select(gr, round, n_pat_notx)
-
-# Crowding out
-res %>% 
-  filter(crowd_out < 0) %>% 
-  select(gr, crowd_out)
-
-
-# Average price
-res %>% 
-  mutate(ppp = budget_impact / n_pat_tx) %>% 
-  group_by(gr) %>% 
-  summarise(mean = mean(ppp, na.rm = T),
-            min = min(ppp, na.rm = T),
-            max = max(ppp, na.rm = T))
-
-res %>% 
-  filter(round == 1) %>% 
-  mutate(ppp = budget_impact / n_pat_tx) %>% 
-  group_by(gr) %>% 
-  summarise(mean = mean(ppp, na.rm = T),
-            min = min(ppp, na.rm = T),
-            max = max(ppp, na.rm = T))
-
-dat_cohort %>% 
-  filter(round == 2) %>% 
-  mutate(ppp = `Budget.impact./.sales` / Patients.treated) %>% 
-  group_by(Group) %>% 
-  summarise(mean = mean(ppp),
-            min = min(ppp),
-            max = max(ppp))
-
-
-
+# 
+# 
+# dat_plot <- res %>% 
+#   pivot_longer(c(gov_total, man_total),
+#                names_to = "Institution",
+#                values_to = "Points") %>% 
+#   mutate(Institution = ifelse(Institution == "gov_total",
+#                               "Government",
+#                               "Pharma"))
+# 
+# # Overall
+# ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
+#   geom_boxplot() +
+#   theme_bw() +
+#   ggtitle("Overall distribution of points")
+# 
+# # Per tutor group
+# ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
+#   geom_boxplot() +
+#   theme_bw() +
+#   facet_wrap(~ wg) +
+#   ggtitle("Overall distribution of points per tutor group")
+# 
+# # Per subgroup
+# ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
+#   geom_boxplot() +
+#   theme_bw() +
+#   facet_wrap(~ gr) +
+#   ggtitle("Overall distribution of points by subgroup")
+# 
+# # Per case
+# ggplot(data = dat_plot, aes(x = Points, y = Institution)) +
+#   geom_boxplot() +
+#   theme_bw() +
+#   facet_grid(round ~ gr) +
+#   ggtitle("Overall distribution of points by case")
+# 
+# res %>% 
+#   arrange(gr) %>% 
+#   select(gr, gov_total, man_total)
+# 
+# # ROUDN toevoegen
+# 
+# # Which group did overspend the government budget?
+# res %>% 
+#   filter(gov_balance < 0) %>% 
+#   group_by(wg, gr) %>% 
+#   summarise(n = n()) 
+# 
+# # Not treated
+# res %>% 
+#   filter(n_pat_notx > 0) %>% 
+#   select(gr, round, n_pat_notx)
+# 
+# # Crowding out
+# res %>% 
+#   filter(crowd_out < 0) %>% 
+#   select(gr, crowd_out)
+# 
+# 
+# # Average price
+# res %>% 
+#   mutate(ppp = budget_impact / n_pat_tx) %>% 
+#   group_by(gr) %>% 
+#   summarise(mean = mean(ppp, na.rm = T),
+#             min = min(ppp, na.rm = T),
+#             max = max(ppp, na.rm = T))
+# 
+# res %>% 
+#   filter(round == 1) %>% 
+#   mutate(ppp = budget_impact / n_pat_tx) %>% 
+#   group_by(gr) %>% 
+#   summarise(mean = mean(ppp, na.rm = T),
+#             min = min(ppp, na.rm = T),
+#             max = max(ppp, na.rm = T))
+# 
+# dat_cohort %>% 
+#   filter(round == 2) %>% 
+#   mutate(ppp = `Budget.impact./.sales` / Patients.treated) %>% 
+#   group_by(Group) %>% 
+#   summarise(mean = mean(ppp),
+#             min = min(ppp),
+#             max = max(ppp))
+# 
+# 
+# 
